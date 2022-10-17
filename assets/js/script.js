a48377e586c86ac5aa4c9fafd8d2e38c2a885459
Carvu94 <cmatej08@gmail.com>
2022-10-17 20:34:23 +0000

    Add JS code to show categories after user selects 'start the quiz' and rearrange html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,13 @@
+let startButton = document.getElementById("start");
+let homepageContainer = document.getElementById("homepage-container");
+let categories = document.getElementById("category-pick");
+
+
+// Wait for the DOM to finish loading
 document.addEventListener("DOMContentLoaded", function() {
+
+    // JS code for modal window and close mostly taken from W3 Schools from: https://www.w3schools.com/howto/howto_css_modals.asp
+    // Modal window opens when user selects 'About us' button
     let aboutUsModal = document.getElementById("about-us-modal");
     let aboutUsButton = document.getElementById("info-button");
     let closeButton = document.getElementsByClassName("close")[0];
@@ -17,4 +26,11 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
+});
+
+// When user selects 'Start the Quiz' categories will be listed
+
+startButton.addEventListener("click", function() {
+    homepageContainer.style.display = "none";
+    categories.style.display = "block";
 });
