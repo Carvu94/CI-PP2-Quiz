a4d955b4656421582e0b530174147fb06eaa8ca4
Carvu94 <cmatej08@gmail.com>
2022-10-18 18:33:38 +0000

    Create a questions js file and add questions for allergens and haccp category

---
+++ assets/js/questions.js
@@ -0,0 +1,123 @@
+let allergensQuestions = [
+    {
+        question: "By European legislation, how many items are listed as allergens?",
+        choices:["15", "14", "16", "10"],
+        answer: 2
+    },
+
+    {
+        question: "Which of the following items contain an allergen?",
+        choices:["Rice Milk", "Honey", "Celeriac", "Linseed"],
+        answer: 3
+    },
+
+    {
+        question: "Which of the following does NOT contain Sulphites?",
+        choices:["Vinegar", "Wine", "Sesame Oil", "Beer",],
+        answer: 3
+    },
+
+    {
+        question: "Choose the item that contains Soybean allergen",
+        choices:["Egg", "Beer", "Milk", "Edamame Bean"],
+        answer: 4
+    },
+
+    {
+        question: "Why is Peanut different allergen then Nuts?",
+        choices:["It grows from ground", "It grows on trees", "It grows by the coast", "It's just special"],
+        answer: 1
+    },
+
+    {
+        question: "Choose the right option that gives the correct allergens for this dish:  'Fries: Potato, Salt, Soybean Oil'.",
+        choices:["Gluten", "Gluten, Soybean", "Soybean", "Milk"],
+        answer: 3
+    },
+
+    {
+        question: "Choose the items that contain Crustaceans allergen",
+        choices:["Crab and Lobster", "Squid and Snail", "Pork and Beef", "Salmon and Trout"],
+        answer: 1
+    },
+
+    {
+        question: "Choose the item that contains Gluten",
+        choices:["Corn", "Potato", "Carrot", "Barley"],
+        answer: 4
+    },
+
+    {
+        question: "Choose the right option that gives the correct allergens for this dish:  'Vegan Cannelloni: Quinoa, Pasta, Mushroom, Courgette, Vinegar, Garlic, Salt, Pepper'",
+        choices:["Gluten", "Gluten, Sulphites", "Gluten, Milk", "Egg"],
+        answer: 2
+    },
+
+    {
+        question: "Which of the following items contain Milk?",
+        choices:["Peanut Butter", "Margarine", "Coconut Milk", "Butter"],
+        answer: 4
+    }
+];
+
+let haccpQuestions = [
+    {
+        question:"What is FIFO?",
+        choices:["Football Federation", "", "First in, First out method", ""],
+        answer: 3
+    },
+
+    {
+        question:"What is critical control point?",
+        choices:["A hazard with an easy solution", "The point in a process where a hazard must be prevented, eliminated or reduced", "Workplace doors", "An area of the premises where food safety is most important"],
+        answer: 2
+    },
+
+    {
+        question:"What is danger zone?",
+        choices:["Temperature of 0 - 5 degrees", "Temperature below -18 degrees", "Temperature above 75 deegrees", "Temperature between 5 and 65 degrees"],
+        answer: 4
+    },
+
+    {
+        question:"What does HACCP stand for?",
+        choices:["Hazard Analysis and Critical Control Point", "Hazard Analysis and Critical Control Probe", "Hazard Analysis and Cooking Control Point", "Hazard Assessment and Critical Control Point"],
+        answer: 1
+    },
+
+    {
+        question:"How many types of contamination are there?",
+        choices:["1", "6", "4", "2"],
+        answer:3
+    },
+
+    {
+        question:"The definition of hazard is...",
+        choices:["Something with the potential to cause harm", "Piece of metal in food", "Mould on food", "Dirt on shoes"],
+        answer: 1
+    },
+
+    {
+        question:"",
+        choices:["", "", "", ""],
+        answer:
+    },
+
+    {
+        question:"",
+        choices:["", "", "", ""],
+        answer:
+    },
+
+    {
+        question:"",
+        choices:["", "", "", ""],
+        answer:
+    },
+
+    {
+        question:"",
+        choices:["", "", "", ""],
+        answer:
+    }
+]
